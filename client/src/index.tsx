6a5220b94165575c290e2976da5ac6b3a6348672
gsk <dfyz0r@list.ru>
2021-03-22 13:28:28 +0300

    feat(client): add react-router-dom and axios

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import store from './redux/store';
 import App from './App';
@@ -8,7 +9,9 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
