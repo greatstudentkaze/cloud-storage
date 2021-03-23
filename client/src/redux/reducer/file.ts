537cfcf7c8c213aec050f306c6faec9c39c64017
gsk <dfyz0r@list.ru>
2021-03-24 00:09:00 +0300

    feat(client/reducer): add action creators and update fileReducer

--- client/src/redux/reducer/file.ts
+++ client/src/redux/reducer/file.ts
@@ -1,9 +1,33 @@
-const initialState = {
+const SET_FILES = 'SET_FILES';
+const SET_CURRENT_DIR = 'SET_CURRENT_DIR';
 
+const initialState = {
+  files: [],
+  currentDirectory: null,
 };
+
+export const setFiles = (files: []) => ({
+  type: SET_FILES,
+  payload: files,
+});
+
+export const setCurrentDirectory = (directory: []) => ({
+  type: SET_CURRENT_DIR,
+  payload: directory,
+});
 
 const fileReducer = (state = initialState, action: any) => {
   switch (action.type) {
+    case SET_FILES:
+      return {
+        ...state,
+        files: action.payload
+      };
+    case SET_CURRENT_DIR:
+      return {
+        ...state,
+        currentDirectory: action.payload
+      };
     default:
       return state;
   }
