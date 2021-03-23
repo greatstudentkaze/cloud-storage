4ab37f9d239451a3bf49775a6de11085e08dc9d3
gsk <dfyz0r@list.ru>
2021-03-23 09:18:01 +0300

    feat(server/models): add File model

---
+++ server/src/models/file.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const fileSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  size: { type: Number, default: 0 },
+  path: { type: String, default: '' },
+  accessLink: { type: String },
+  parent: { type: ObjectId, ref: 'File' },
+  user: { type: ObjectId, ref: 'User' },
+  children: [{ type: ObjectId, ref: 'File' }],
+});
+
+export default mongoose.model('File', fileSchema);
