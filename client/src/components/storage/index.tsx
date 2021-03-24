19f05eed908a9794b81fdeb5030786b51d699376
gsk <dfyz0r@list.ru>
2021-03-24 09:42:06 +0300

    feat(client/Storage): add drag&drop file upload

--- client/src/components/storage/index.tsx
+++ client/src/components/storage/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect } from 'react';
+import React, { ChangeEvent, DragEventHandler, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { RootState } from '../../redux/store';
@@ -12,6 +12,7 @@ const Storage = () => {
   const dispatch = useDispatch();
   const currentDirectory = useSelector(({ file }: RootState) => file.currentDirectory);
   const dirStack = useSelector(({ file }: RootState) => file.dirStack);
+  const [dragEnter, setDragEnter] = useState(false);
 
   useEffect(() => {
     dispatch(getFiles(currentDirectory));
@@ -34,21 +35,54 @@ const Storage = () => {
     files.forEach(file => dispatch(uploadFile(file, currentDirectory)));
   }
 
+  const handleDragEnter: DragEventHandler<HTMLDivElement> = (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    setDragEnter(true);
+  };
+
+  const handleDragLeave: DragEventHandler<HTMLDivElement> = (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    setDragEnter(false);
+  };
+
+  const handleDrop: DragEventHandler<HTMLDivElement> = (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    const fileList = evt.dataTransfer.files;
+
+    const files = fileList ? [...Array.from(fileList)] : [];
+    files.forEach(file => dispatch(uploadFile(file, currentDirectory)));
+
+    setDragEnter(false);
+  };
+
   return (
-    <div className="storage">
-      <div className="storage__buttons">
-        {
-          currentDirectory && <button className="storage__button storage__button--back" type="button" onClick={handleBackButtonClick}>Назад</button>
-        }
-        <button className="storage__button storage__button--create" type="button" onClick={handleCreateClick}>Создать папку</button>
-        <div className="storage__upload">
-          <label htmlFor="upload" className="storage__upload-label">Загрузить файл</label>
-          <input className="storage__upload-input" type="file" id="upload" onChange={handleFileUploadChange} multiple={true} />
-        </div>
+    dragEnter
+      ? (
+      <div className="drop-area" onDrop={handleDrop} onDragEnter={handleDragEnter} onDragLeave={handleDragLeave} onDragOver={handleDragEnter}>
+        Перетащите файлы сюда
       </div>
-      <FileList />
-      <Popup />
-    </div>
+      )
+      : (
+        <div className="storage" onDragEnter={handleDragEnter} onDragLeave={handleDragLeave} onDragOver={handleDragEnter} >
+          <div className="storage__buttons">
+            {
+              currentDirectory && <button className="storage__button storage__button--back" type="button" onClick={handleBackButtonClick}>Назад</button>
+            }
+            <button className="storage__button storage__button--create" type="button" onClick={handleCreateClick}>Создать папку</button>
+            <div className="storage__upload">
+              <label htmlFor="upload" className="storage__upload-label">Загрузить файл</label>
+              <input className="storage__upload-input" type="file" id="upload" onChange={handleFileUploadChange} multiple={true} />
+            </div>
+          </div>
+          <FileList />
+          <Popup />
+        </div>
+      )
   );
 };
 
