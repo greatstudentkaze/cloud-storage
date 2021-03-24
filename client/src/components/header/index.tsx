2e85da22c3a532418c0e4721a19a772c744b770a
gsk <dfyz0r@list.ru>
2021-03-24 20:07:54 +0300

    feat(client): add avatar upload and delete

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -12,16 +12,19 @@ import Input from '../input';
 import './css/user-list.css';
 import './css/main-nav.css';
 import './css/page-header.css';
+import userAvatar from '../../assets/img/user-avatar.svg';
 
 const SEARCH_TIMEOUT = 500;
 
 const Header = () => {
   const isAuthorized = useSelector(({ user }: RootState) => user.isAuthorized);
   const currentDirectory = useSelector(({ file }: RootState) => file.currentDirectory);
+  const currentUser = useSelector(({ user }: RootState) => user.currentUser);
   const sort = useSelector(({ file }: RootState) => file.sort);
   const dispatch = useDispatch();
   const [searchQuery, setSearchQuery] = useState('');
   const [searchTimeout, setSearchTimeout] = useState<NodeJS.Timeout>(null as unknown as NodeJS.Timeout);
+  const avatar = currentUser.avatar ? `http://localhost:9111/${currentUser.avatar}` : userAvatar;
 
   useEffect(() => {
     if (searchTimeout) {
@@ -61,6 +64,11 @@ const Header = () => {
           {
             isAuthorized && <li className="user-list__item">
               <Input className="user-list__search" type="text" placeholder="Название файла..." value={searchQuery} setValue={setSearchQuery} />
+            </li>
+          }
+          {
+            isAuthorized && <li className="user-list__item">
+              <NavLink className="user-list__profile" to="/profile"><img src={avatar} width="30" height="30" alt="Аватар" /></NavLink>
             </li>
           }
           {
